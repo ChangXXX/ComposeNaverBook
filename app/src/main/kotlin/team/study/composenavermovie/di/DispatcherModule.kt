bcaaf3b8914353967e8caa8471659fc39219c733
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-01-31 23:35:38 +0900

    [ADD] add a DispatcherModule

---
+++ app/src/main/kotlin/team/study/composenavermovie/di/DispatcherModule.kt
@@ -0,0 +1,27 @@
+package team.study.composenavermovie.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+import kotlin.annotation.AnnotationRetention.RUNTIME
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+
+    @Provides
+    @Dispatcher(CustomDispatchers.IO)
+    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
+
+@Qualifier
+@Retention(RUNTIME)
+annotation class Dispatcher(val customDispatcher: CustomDispatchers)
+
+enum class CustomDispatchers {
+    IO, DEFAULT
+}
