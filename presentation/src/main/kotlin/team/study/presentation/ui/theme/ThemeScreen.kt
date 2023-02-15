d6a874fc4e6295e474e0b5f6ff8873f1f3de14d1
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-02-15 22:47:52 +0900

    [ADD] ThemeCard 디자인 구현

--- presentation/src/main/kotlin/team/study/presentation/ui/theme/ThemeScreen.kt
+++ presentation/src/main/kotlin/team/study/presentation/ui/theme/ThemeScreen.kt
@@ -1,11 +1,21 @@
 package team.study.presentation.ui.theme
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ThemeScreen() {
@@ -13,6 +23,45 @@ fun ThemeScreen() {
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(text = "ThemeScreen")
+        ThemeCard("Dark Mode", {})
+        ThemeCard("Light Mode", {})
+        ThemeCard("Follow System", {})
+    }
+}
+
+@Composable
+private fun ThemeCard(
+    title: String,
+    onClickTheme: () -> Unit,
+) {
+    OutlinedCard(
+        modifier = Modifier
+            .padding(12.dp)
+            .height(70.dp)
+            .fillMaxWidth(),
+        colors = CardDefaults.outlinedCardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = MaterialTheme.colorScheme.secondary,
+        ),
+        border = CardDefaults.outlinedCardBorder(true),
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineMedium,
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Checkbox(
+                checked = true,
+                onCheckedChange = {},
+                modifier = Modifier
+                    .fillMaxHeight(),
+            )
+        }
     }
 }
