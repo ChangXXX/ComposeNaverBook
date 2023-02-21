1e1b38abad5ca7c69900c0e10df07a650fe08715
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-02-22 00:51:14 +0900

    [ADD] CircularProgressBar 생성

---
+++ presentation/src/main/kotlin/team/study/presentation/ui/components/NaverMovieProgress.kt
@@ -0,0 +1,25 @@
+package team.study.presentation.ui.components
+
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+
+@Composable
+fun CircularProgressAnimated() {
+    val transition = rememberInfiniteTransition()
+    val progressAnimationValue by transition.animateFloat(
+        initialValue = 0.0f,
+        targetValue = 1.0f,
+        animationSpec = infiniteRepeatable(animation = tween(900), RepeatMode.Restart),
+    )
+    CircularProgressIndicator(
+        progress = progressAnimationValue,
+        color = MaterialTheme.colorScheme.primaryContainer,
+    )
+}
