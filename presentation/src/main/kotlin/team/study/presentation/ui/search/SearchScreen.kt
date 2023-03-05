3147e12406ec92044ce302919359399e0f1c7a5a
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-03-05 23:12:15 +0900

    [UPDATE] SearchBar valuechanged 추가

--- presentation/src/main/kotlin/team/study/presentation/ui/search/SearchScreen.kt
+++ presentation/src/main/kotlin/team/study/presentation/ui/search/SearchScreen.kt
@@ -3,8 +3,13 @@ package team.study.presentation.ui.search
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import team.study.presentation.ui.components.NaverBookSearchBar
 
 @Composable
@@ -13,10 +18,25 @@ fun SearchScreen() {
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        var searchValue by remember { mutableStateOf("") }
+        val view = LocalFocusManager.current
+
         NaverBookSearchBar(
             modifier = Modifier,
-            value = "",
-            onValueChanged = {},
+            value = searchValue,
+            onClear = {
+                searchValue = ""
+                view.clearFocus()
+            },
+            onDone = {
+                view.clearFocus()
+            },
+            onFocusChanged = { focusState ->
+                focusState.isFocused
+            },
+            onValueChanged = { query ->
+                searchValue = query
+            },
         )
     }
 }
