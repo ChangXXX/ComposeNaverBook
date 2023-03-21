a68fc0c4fdaf6e8fb4ad6f8816f59e89f1ec5d37
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-03-21 21:43:59 +0900

    [ADD] HeaderInterceptor.kt 추가로 인한 헤더 어노테이션 제거

--- data/src/main/java/team/study/data/network/BookService.kt
+++ data/src/main/java/team/study/data/network/BookService.kt
@@ -2,19 +2,13 @@ package team.study.data.network
 
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
-import team.study.data.BuildConfig
 import team.study.data.model.SearchResponse
 
 interface BookService {
 
     @GET("v1/search/book.json")
-    @Headers(
-        "X-Naver-Client-Id: ${BuildConfig.CLIENT_ID}",
-        "X-Naver-Client_Secret: ${BuildConfig.API_KEY}",
-    )
-    suspend fun getSearchBooks(
+    suspend fun searchBooks(
         @Query("query") query: String,
     ): Response<SearchResponse>
 }
