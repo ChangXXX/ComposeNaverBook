fd3baa4aa9028928f854621af103a891a2eedb1a
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-03-23 23:10:43 +0900

    [REFACTOR] NetworkResult add override toString

--- data/src/main/java/team/study/data/model/NetworkResult.kt
+++ data/src/main/java/team/study/data/model/NetworkResult.kt
@@ -1,8 +1,31 @@
 package team.study.data.model
 
 sealed class NetworkResult<out T> {
-    data class Success<T>(val data: T) : NetworkResult<T>()
-    data class ApiError(val code: Int, val message: String?) : NetworkResult<Nothing>()
-    data class NetworkError(val e: Throwable) : NetworkResult<Nothing>()
-    data class UnknownError(val e: Throwable?) : NetworkResult<Nothing>()
+    data class Success<T>(val data: T) : NetworkResult<T>() {
+
+        override fun toString(): String {
+            return "Success data : $data"
+        }
+    }
+
+    data class ApiError(val code: Int, val message: String?) : NetworkResult<Nothing>() {
+
+        override fun toString(): String {
+            return "Error code : $code / message : $message"
+        }
+    }
+
+    data class NetworkError(val e: Throwable) : NetworkResult<Nothing>() {
+
+        override fun toString(): String {
+            return "NetworkError : $e"
+        }
+    }
+
+    data class UnknownError(val e: Throwable?) : NetworkResult<Nothing>() {
+
+        override fun toString(): String {
+            return "UnknownError : $e"
+        }
+    }
 }
