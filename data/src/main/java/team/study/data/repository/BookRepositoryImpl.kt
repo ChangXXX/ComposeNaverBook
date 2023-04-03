328f52d9da042b795d3cffc267e3e99a703b940e
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-04-04 01:07:34 +0900

    [REFACTOR] fun search 구현부 수정

--- data/src/main/java/team/study/data/repository/BookRepositoryImpl.kt
+++ data/src/main/java/team/study/data/repository/BookRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package team.study.data.repository
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import team.study.data.datasource.BookRemoteDataSource
-import team.study.data.model.NetworkResult
+import team.study.data.model.ApiResponse
+import team.study.data.network.di.CustomDispatchers
+import team.study.data.network.di.Dispatcher
 import team.study.data.util.mapper.toDomain
 import team.study.domain.model.Book
 import team.study.domain.repository.BookRepository
@@ -12,30 +16,27 @@ import javax.inject.Inject
 
 class BookRepositoryImpl @Inject constructor(
     private val bookRemoteDataSource: BookRemoteDataSource,
+    @Dispatcher(CustomDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
 ) : BookRepository {
 
     override suspend fun search(
         query: String,
+        onError: (String?) -> Unit,
     ): Flow<List<Book>> = flow {
         bookRemoteDataSource.searchBooks(query).let { result ->
             when (result) {
-                is NetworkResult.Success -> {
-                    emit(
-                        result.data.items.map { searchItem ->
-                            searchItem.toDomain()
-                        },
-                    )
+                is ApiResponse.Success -> {
+                    emit(result.data.items.map { it.toDomain() })
                 }
-                is NetworkResult.ApiError -> {
-                    Timber.e(result.toString())
+                is ApiResponse.Failure.Error -> {
+                    Timber.d(result.toString())
+                    onError(null)
                 }
-                is NetworkResult.NetworkError -> {
-                    Timber.e(result.toString())
-                }
-                is NetworkResult.UnknownError -> {
-                    Timber.e(result.toString())
+                is ApiResponse.Failure.Exception -> {
+                    Timber.d(result.toString())
+                    onError(null)
                 }
             }
         }
-    }
+    }.flowOn(ioDispatcher)
 }
