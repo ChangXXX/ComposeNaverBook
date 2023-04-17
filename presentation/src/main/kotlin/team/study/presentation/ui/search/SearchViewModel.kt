6b7e796897886000548ed777e2018cd4ad11bde0
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-04-18 01:00:43 +0900

    [REFACTOR] 검색어 query.toString() -> uiState.value.query 사용 변경

--- presentation/src/main/kotlin/team/study/presentation/ui/search/SearchViewModel.kt
+++ presentation/src/main/kotlin/team/study/presentation/ui/search/SearchViewModel.kt
@@ -6,6 +6,8 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import team.study.domain.model.Book
@@ -23,6 +25,7 @@ class SearchViewModel @Inject constructor(
     val books = uiState.map { it.books }
     val isError = uiState.map { it.isError }
     val toastMessage = uiState.map { it.toastMessage }
+    val isLoading = uiState.map { it.isLoading }
 
     fun onChangedQuery(query: String) {
         uiState.update {
@@ -32,9 +35,9 @@ class SearchViewModel @Inject constructor(
 
     fun search() {
         viewModelScope.launch {
-            Timber.tag("INIT SEARCH ::").d(query.toString())
+            Timber.tag("INIT SEARCH ::").d(uiState.value.query)
             bookSearchUseCase.invoke(
-                query = query.toString(),
+                query = uiState.value.query,
                 onException = { msg ->
                     val exceptionMsg = msg ?: "알 수 없는 에러"
                     uiState.update {
@@ -42,6 +45,8 @@ class SearchViewModel @Inject constructor(
                     }
                 },
             )
+                .onStart { uiState.update { it.copy(isLoading = true) } }
+                .onCompletion { uiState.update { it.copy(isLoading = false) } }
                 .collectLatest { books ->
                     uiState.update { it.copy(books = books) }
                 }
